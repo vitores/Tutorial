1c455ab65272c32e4ca2a3420c5cc81789ece9aa
vitores <josef.sanchezvitores@gmail.com>
2018-01-30 23:23:26 +0100

    abcde

--- December/src/prueba/sonar/git/jfsv/TestJFSV.java
+++ December/src/prueba/sonar/git/jfsv/TestJFSV.java
@@ -4,13 +4,10 @@ public class TestJFSV {
 
     public static void main(String[] args) {
 
-<<<<<<< HEAD
+
         System.out.println(" world and  jose francisco");
    
-=======
-        System.out.println(" world and dfs jose francisco");
 
->>>>>>> branch 'master' of https://github.com/vitores/Tutorial
     }
 
 }
