e8178120816efb6b91d798ea9ea9f7d5166d05d8
vitores <josef.sanchezvitores@gmail.com>
2018-01-30 23:31:30 +0100

    abc

--- December/src/prueba/sonar/git/jfsv/TestJFSV.java
+++ December/src/prueba/sonar/git/jfsv/TestJFSV.java
@@ -3,6 +3,8 @@ package src.prueba.sonar.git.jfsv;
 public class TestJFSV {
 
     public static void main(String[] args) {
+    	String kk = "";
+    	
         System.out.println(" world and  jose francisco");
     }
 
