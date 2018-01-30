018500bd6764b36c0bbfbddbc7e393c10bd10f34
vitores <josef.sanchezvitores@gmail.com>
2018-01-30 23:30:25 +0100

    Update TestJFSV.java

--- December/src/prueba/sonar/git/jfsv/TestJFSV.java
+++ December/src/prueba/sonar/git/jfsv/TestJFSV.java
@@ -4,6 +4,8 @@ public class TestJFSV {
 
     public static void main(String[] args) {
         System.out.println(" world and  jose francisco");
+        
+        String cadena = "esto es una prueba";
     }
 
 }
