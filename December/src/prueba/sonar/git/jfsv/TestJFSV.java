5c4a4881a89a68c63348753c5bb0de79c5e82a54
josef <josef.sanchezvitores@gmail.com>
2018-02-08 11:59:25 +0100

    commit to my workspace

--- December/src/prueba/sonar/git/jfsv/TestJFSV.java
+++ December/src/prueba/sonar/git/jfsv/TestJFSV.java
@@ -1,19 +1,23 @@
 package src.prueba.sonar.git.jfsv;
 
 public class TestJFSV {
-
+    
     public static void main(String[] args) {
-<<<<<<< HEAD
-    	String kk = "";
-
+        String kk = "";
+        
         System.out.println(" world and  jose francisco");
         
         String cadena = "esto es una prueba";
         
         cadena = "";
-
+        
     }
     
-    public void kk();
-
+    public static void kk() {
+    }
+    
+    
+    publi boolean exits() {
+        return true;
+    }
 }
