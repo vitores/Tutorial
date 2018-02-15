3633df931859e12aa39ce1ef0f6a7d6eb1ff67c2
josef <josef.sanchezvitores@gmail.com>
2018-02-15 15:57:07 +0100

    change for the other test

--- December/src/prueba/sonar/git/jfsv/TestJFSV.java
+++ December/src/prueba/sonar/git/jfsv/TestJFSV.java
@@ -3,7 +3,7 @@ package src.prueba.sonar.git.jfsv;
 public class TestJFSV {
     
     public static void main(String[] args) {
-        String cadena = "test";
+        String cadena = "other test";
         System.out.println("Valor: " + cadena);
     }
     
