83d2e73faad751127ffe3d91cd7bb304cafc5bf9
josef <josef.sanchezvitores@gmail.com>
2018-02-15 08:45:17 +0100

    definitive version

--- December/src/prueba/sonar/git/jfsv/TestJFSV.java
+++ December/src/prueba/sonar/git/jfsv/TestJFSV.java
@@ -3,13 +3,8 @@ package src.prueba.sonar.git.jfsv;
 public class TestJFSV {
     
     public static void main(String[] args) {
-        String kk = "";
         
         System.out.println(" world and  jose francisco");
-        
-        String cadena = "esto es una prueba";
-        
-        cadena = "";
         
     }
     
