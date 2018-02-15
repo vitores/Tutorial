8bee2a99e522e3c26e8aa0eec6b7092507123452
josef <josef.sanchezvitores@gmail.com>
2018-02-15 08:50:10 +0100

    definite version 2

--- December/src/prueba/sonar/git/jfsv/TestJFSV.java
+++ December/src/prueba/sonar/git/jfsv/TestJFSV.java
@@ -3,11 +3,8 @@ package src.prueba.sonar.git.jfsv;
 public class TestJFSV {
     
     public static void main(String[] args) {
-        
         System.out.println(" world and  jose francisco");
-        
     }
-   
     
     public boolean exits() {
         return true;
