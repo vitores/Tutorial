c0728820175fb684f23fbe5a6fa03ca75a854639
josef <josef.sanchezvitores@gmail.com>
2018-02-15 08:59:56 +0100

    new version

--- December/src/prueba/sonar/git/jfsv/TestJFSV.java
+++ December/src/prueba/sonar/git/jfsv/TestJFSV.java
@@ -3,12 +3,8 @@ package src.prueba.sonar.git.jfsv;
 public class TestJFSV {
     
     public static void main(String[] args) {
-        String cadena = "prueba";
+        String cadena = "jose";
         System.out.println("Valor: " + cadena);
-    }
-    
-    public boolean exits() {
-        return true;
     }
     
 }
